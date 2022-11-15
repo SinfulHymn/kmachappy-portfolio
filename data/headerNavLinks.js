e38f95d35a5f4f1fe6a6a7c70ff8fa5a96aed73e
Alejandro Rojas <kmachappy@pm.me>
2022-11-15 11:26:51 -0800

    updating style

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
+  { href: '/blog', title: 'Blog' },
   { href: '/about', title: 'About' },
 ]
 
