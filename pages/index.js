7a4133957b683834d42f9c22370d77af84f526cb
Alejandro Rojas <kmachappy@pm.me>
2022-11-16 13:14:52 -0800

    more styling changes

--- pages/index.js
+++ pages/index.js
@@ -34,9 +34,14 @@ export default function Home({ posts, projects }) {
           </p>
         </div>
 
-        <div>
-          <div className="py-2 text-2xl font-semibold">Recent Projects</div>
-          <div className="grid grid-cols-1 gap-4  md:grid-cols-2 xl:grid-cols-3">
+        <div className="py-2">
+          <div className="flex justify-between py-2 text-2xl font-semibold">
+            <div>Recent Projects</div>
+            <Link href={'/projects'} className="text-xl">
+              See More {'->'}
+            </Link>
+          </div>
+          <div className="grid grid-cols-1 gap-4 py-4  md:grid-cols-2 xl:grid-cols-3">
             {projectsData.slice(0, MAX_DISPLAY).map((d) => (
               <Card
                 key={d.title}
@@ -48,59 +53,60 @@ export default function Home({ posts, projects }) {
             ))}
           </div>
         </div>
-
-        <ul className="divide-y divide-primary-500 dark:divide-primary-600">
-          <div className="py-2 text-2xl font-semibold">Recent Projects</div>
-          {!posts.length && notFound()}
-          {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
-            const { slug, date, title, summary, tags } = frontMatter
-            return (
-              <li key={slug} className="py-6">
-                <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
-                    <dl>
-                      <dt className="sr-only">Published on</dt>
-                      <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                        <time dateTime={date}>{formatDate(date)}</time>
-                      </dd>
-                    </dl>
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
-                        <div>
-                          <h2 className="text-2xl font-bold leading-8 tracking-tight">
-                            <Link
-                              href={`/blog/${slug}`}
-                              className="text-gray-900 dark:text-gray-100"
-                            >
-                              {title}
-                            </Link>
-                          </h2>
-                          <div className="flex flex-wrap">
-                            {tags.map((tag) => (
-                              <Tag key={tag} text={tag} />
-                            ))}
+        <div className="py-2">
+          <div className="py-2 text-2xl font-semibold">Recent Posts</div>
+          <ul className="divide-y divide-primary-500 dark:divide-primary-600">
+            {!posts.length && notFound()}
+            {posts.slice(0, MAX_DISPLAY).map((frontMatter) => {
+              const { slug, date, title, summary, tags } = frontMatter
+              return (
+                <li key={slug} className="py-4">
+                  <article>
+                    <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-baseline xl:space-y-0">
+                      <dl>
+                        <dt className="sr-only">Published on</dt>
+                        <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+                          <time dateTime={date}>{formatDate(date)}</time>
+                        </dd>
+                      </dl>
+                      <div className="space-y-5 xl:col-span-3">
+                        <div className="space-y-6">
+                          <div>
+                            <h2 className="text-2xl font-bold leading-8 tracking-tight">
+                              <Link
+                                href={`/blog/${slug}`}
+                                className="text-gray-900 dark:text-gray-100"
+                              >
+                                {title}
+                              </Link>
+                            </h2>
+                            <div className="flex flex-wrap">
+                              {tags.map((tag) => (
+                                <Tag key={tag} text={tag} />
+                              ))}
+                            </div>
+                          </div>
+                          <div className="prose max-w-none text-gray-500 dark:text-gray-400">
+                            {summary}
                           </div>
                         </div>
-                        <div className="prose max-w-none text-gray-500 dark:text-gray-400">
-                          {summary}
+                        <div className="text-base font-medium leading-6">
+                          <Link
+                            href={`/blog/${slug}`}
+                            className="text-primary-500 hover:text-primary-600 dark:text-primary-600 dark:hover:text-primary-500"
+                            aria-label={`Read "${title}"`}
+                          >
+                            Read more &rarr;
+                          </Link>
                         </div>
                       </div>
-                      <div className="text-base font-medium leading-6">
-                        <Link
-                          href={`/blog/${slug}`}
-                          className="text-primary-500 hover:text-primary-600 dark:text-primary-600 dark:hover:text-primary-500"
-                          aria-label={`Read "${title}"`}
-                        >
-                          Read more &rarr;
-                        </Link>
-                      </div>
                     </div>
-                  </div>
-                </article>
-              </li>
-            )
-          })}
-        </ul>
+                  </article>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
       </div>
       {posts.length > MAX_DISPLAY && (
         <div className="flex justify-end text-base font-medium leading-6">
