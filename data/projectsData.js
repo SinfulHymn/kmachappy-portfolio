f70a523094a105b8c8c3d0ec7fb810676fea67da
Alejandro Rojas <kmachappy@pm.me>
2022-11-22 13:29:54 -0800

    description changes

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: 'Twisted Dreams Haunt',
-    description: ``,
+    description: `Twisted Dreams Haunt is a client website for a local haunted house. The site was built using Next.js and Tailwind CSS. The site is hosted on Vercel and uses a custom CMS built with Next.js and postgresql`,
     imgSrc: '/static/images/TwistedDreams.jpg',
     href: 'https://www.twisteddreamshaunt.com/',
     date: 'October 2022',
@@ -9,7 +9,7 @@ const projectsData = [
   },
   {
     title: 'Big Guy Auto',
-    description: ``,
+    description: `Big Guy Auto is a client website for a local auto repair shop. The site is built with Next.js and Tailwind CSS. The site is hosted on Vercel and uses a headless CMS called Strapi.`,
     imgSrc: '/static/images/BigGuy.jpg',
     href: 'https://www.bigguyautobody.com/',
     date: 'September 2022',
@@ -17,7 +17,7 @@ const projectsData = [
   },
   {
     title: 'AlgoVisualizer',
-    description: ``,
+    description: `AlgoVisualizer is a web application that allows users to visualize sorting algorithms. It is built with React and Tailwind CSS.`,
     imgSrc: '/static/images/AlgoVisualizer.png',
     href: 'https://algorithm-visuals.netlify.app/',
     date: 'August 2022',
@@ -25,7 +25,7 @@ const projectsData = [
   },
   {
     title: 'KmacShop',
-    description: ``,
+    description: `KmacShop is a Django E-commerce site that allows users to buy and sell items. It also allows users to create their own stores and sell their items. This project was created to help me learn the Django framework and to help me learn how to deploy a Django app to Heroku.`,
     imgSrc: '/static/images/KmacShop.jpg',
     href: 'https://kmacshop.herokuapp.com/',
     date: 'July 2022',
@@ -33,7 +33,7 @@ const projectsData = [
   },
   {
     title: 'Therapist Finder',
-    description: `⁜ Therapist Finder is a Cloud Database directory of counseling providers. Users are able able to browse a directory of counseling providers, and choose to view specific providers detailed information. Users are able to authenticate with Google OAuth and are given the functionality of being able to leave reviews on individual providers. Users are also granted full CRUD functionality on their reviews.`,
+    description: `Therapist Finder is a Cloud Database directory of counseling providers. Users are able able to browse a directory of counseling providers, and choose to view specific providers detailed information. Users are able to authenticate with Google OAuth and are given the functionality of being able to leave reviews on individual providers. Users are also granted full CRUD functionality on their reviews.`,
     imgSrc: '/static/images/TherapistFinder.png',
     href: 'https://therapist-finder.netlify.app/',
     date: 'June 2022',
@@ -41,7 +41,7 @@ const projectsData = [
   },
   {
     title: 'Runners Page',
-    description: `⁜ Runners page is a Cloud Database directory of running race events. Users are able to browse running events and view details about upcoming races such as marathons as well as leave reviews. Users are able to login with Google OAuth and have full CRUD function on event data and event reviews.`,
+    description: `Runners page is a Cloud Database directory of running race events. Users are able to browse running events and view details about upcoming races such as marathons as well as leave reviews. Users are able to login with Google OAuth and have full CRUD function on event data and event reviews.`,
     imgSrc: '/static/images/RunnersPage.jpg',
     href: 'https://runners-page.herokuapp.com/',
     date: 'May 2022',
@@ -57,7 +57,7 @@ const projectsData = [
   },
   {
     title: 'Marathon View',
-    description: `⁜ Athletes Activity Page is a Web App that gathers athletic data from the Strava api. Using the Strava data, the data is displayed graphically and lists all activity statistics for the user to analyze. The strava api is used in conjunction with the Leaflet api to generate a map to display the provided strava polyline data to show a route summary of the chosen activity displayed`,
+    description: `Athletes Activity Page is a Web App that gathers athletic data from the Strava api. Using the Strava data, the data is displayed graphically and lists all activity statistics for the user to analyze. The strava api is used in conjunction with the Leaflet api to generate a map to display the provided strava polyline data to show a route summary of the chosen activity displayed`,
     imgSrc: '/static/images/MarathonView.jpg',
     href: 'https://kmachappy.github.io/',
     date: 'April 2022',
