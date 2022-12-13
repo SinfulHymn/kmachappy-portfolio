eddf2afaa2af7a688ad67306abfd2ed528de3b1e
Alejandro Rojas <kmachappy@pm.me>
2022-12-13 12:37:32 -0800

    test

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -3,7 +3,10 @@ const headerNavLinks = [
   { href: '/projects', title: 'Projects' },
   { href: '/blog', title: 'Blog' },
   { href: '/about', title: 'About' },
-  { href: '/static/Alejandro_Rojas_Resume.pdf', title: 'Resume' },
+  {
+    href: 'https://docs.google.com/document/d/e/2PACX-1vS3SN3_m9eUs95gx1H_kwM8j-WmdDa_MuxLd83JPUlxmeAFp0tXuEuuMGgRsvwpUTIXc2owHAL-q_jf/pub',
+    title: 'Resume',
+  },
 ]
 
 export default headerNavLinks
