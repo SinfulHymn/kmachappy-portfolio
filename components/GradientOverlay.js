ebeb3834f39ae7f96c03a40b1429b1b125c76abb
Alejandro Rojas <kmachappy@pm.me>
2023-01-12 11:00:39 -0800

    added priority loading to gradient overlay

--- components/GradientOverlay.js
+++ components/GradientOverlay.js
@@ -9,6 +9,7 @@ export default function GradientOverlay() {
           alt="Picture of the author"
           width={1000}
           height={800}
+          priority
         ></Image>
       </div>
       <div className="fixed -top-96 right-64 -z-10 h-full w-full   opacity-100 sm:-right-1/4">
@@ -17,6 +18,7 @@ export default function GradientOverlay() {
           alt="Picture of the author"
           width={1000}
           height={800}
+          priority
         ></Image>
       </div>
       <div className="fixed -bottom-11 -left-28 -z-10 h-full w-full opacity-100 sm:-bottom-[30%] sm:-left-[7%]">
@@ -25,6 +27,7 @@ export default function GradientOverlay() {
           alt="Picture of the author"
           width={800}
           height={800}
+          priority
         ></Image>
       </div>
       <div className="fixed -top-64 right-64 -z-10 h-full w-full opacity-100 sm:top-[37%] md:-right-[40%]">
@@ -33,6 +36,7 @@ export default function GradientOverlay() {
           alt="Picture of the author"
           width={800}
           height={800}
+          priority
         ></Image>
       </div>
       <div className="fixed top-96 right-64 -z-10 h-full w-full opacity-100 sm:-right-3/4">
@@ -41,6 +45,7 @@ export default function GradientOverlay() {
           alt="Picture of the author"
           width={1000}
           height={800}
+          priority
         ></Image>
       </div>
     </>
