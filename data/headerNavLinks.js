a13c59d83901a6893f996d685ddc0e09488fed42
Alejandro Rojas <kmachappy@pm.me>
2023-03-14 03:30:18 -0700

    updated resume link

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -4,7 +4,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/about', title: 'About' },
   {
-    href: 'https://drive.google.com/file/d/1kJaVTw8-K5T3-mHM45Jkf6S1j1wv28Wp/view?usp=sharing',
+    href: 'https://drive.google.com/file/d/1hOX2AwAAMHk--CkW56HQlJkOPihjQdCh/view?usp=sharing',
     title: 'Resume',
   },
 ]
