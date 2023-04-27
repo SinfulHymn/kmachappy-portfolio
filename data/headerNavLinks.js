f06f7c5917bd4d60704c7253b4cae7829b000033
Alejandro Rojas <contact@kmachappy.me>
2023-04-27 01:18:59 -0700

    upodate

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -4,7 +4,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/about', title: 'About' },
   {
-    href: 'https://drive.google.com/file/d/1hOX2AwAAMHk--CkW56HQlJkOPihjQdCh/view?usp=sharing',
+    href: 'https://drive.google.com/file/d/115JX8WCZJnAJRW9NksrYPEO_fdMSqHKd/view?usp=sharing',
     title: 'Resume',
   },
 ]
